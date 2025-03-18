6041f850ae79cd35028fd829af201887a61b7c70
Piyush <piyushpatil0031@gmail.com>
2025-03-19 01:29:39 +0530

    added signup page and signin page and its functionality

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+const ejs = require('ejs');
+const userRoute = require('./routes/user');
+const { json } = require('body-parser');
+const mongoose = require('mongoose');
+
+
 app.set('view engine', 'ejs');
 
 const port = 3000;
+
+mongoose.connect('mongodb://localhost:27017/BlogTV').then(() => console.log("mongodb connected"));
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
+
+
+app.use('/user',userRoute);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
 app.get('/', (req, res) => {
-    res.render('index');
-    console.log('Home page');
+    res.render('home');
 });
 
 app.get('/create-blog', (req, res) => {
     res.render('create-blog');
-    console.log('creating a blog');
 });
 
 app.get('/edit-blog', (req, res) => {
     res.render('edit-blog');
-    console.log('editing a blog');
 });
 
 app.get('/delete-blog', (req, res) => {
     res.render('delete-blog');
-    console.log('deleting a blog');
 });
 
 app.get('*', (req, res) => {
     res.render('error');
-    console.log('error - page not found');
 });
+
+
---
+++ models/user.js
@@ -0,0 +1,66 @@
+const { Schema, model, default: mongoose } = require('mongoose');
+const crypto = require('crypto');
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    salt: {
+        type: String,
+    },
+    pofileImageURL: {
+        type: String,
+        default: "../public/images/default.jpg",
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+    },
+
+}, { timestamps: true });
+
+
+
+userSchema.static("matchPassword", async function(email, password){
+    const user = await User.findOne({email});
+    if(!user) throw new Error("User not found!!");
+
+    const salt = user.salt;
+    const hashedPassword = user.password;
+
+    const userProvidedHash = crypto.createHmac("sha256", salt).update(password).digest("hex");
+
+    if(hashedPassword !== userProvidedHash)
+        throw new Error("Wrong Password!!");
+
+    return user;
+});
+
+userSchema.pre('save', function (next) {
+    const user = this;
+    if (!user.isModified("password")) return;
+
+    const salt = crypto.randomBytes(16).toString();
+    const hashedPassword = crypto.createHmac("sha256", salt).update(user.password).digest("hex");
+
+    this.salt = salt;
+    this.password = hashedPassword;
+    
+    next();
+});
+
+
+const User = model("user", userSchema);
+
+module.exports = User;
